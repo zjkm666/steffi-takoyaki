ad92956682238aabd68c8df644f2dc5c760f029e
BitLJuice <joshuaklein.malonda@gmail.com>
2025-12-26 14:31:33 +0800

    fixed bugs

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ class Raindrop {
         this.y = Math.random() * window.innerHeight - window.innerHeight;
         this.width = Math.random() * 20 + 20; // 8-16px
         this.height = this.width * 20;
-        this.speed = Math.random() * 1 + 1; // 6-10px per frame
+        this.speed = Math.random() * 1 + 3; // 6-10px per frame
         this.windEffect = Math.random() * 2 - 1; // -1 to 1
 
         this.element.style.width = this.width + 'px';
